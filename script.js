164aedd0e6db7482b24ac8fad760a4e71eb94545
viivek46 <39732335+viivek46@users.noreply.github.com>
2018-09-13 12:13:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-rs.conf()
+rs.initiate( {
+  _id : "rs0",
+  members: [
+     { _id: 0, host: "mongodb-0-mongod.mongodbservice.autoip.dcos.thisdcos.directory:27017" },
+     { _id: 1, host: "mongodb-1-mongod.mongodbservice.autoip.dcos.thisdcos.directory:27017" },
+     { _id: 2, host: "mongodb-2-mongod.mongodbservice.autoip.dcos.thisdcos.directory:27017" }
+  ]
+})
